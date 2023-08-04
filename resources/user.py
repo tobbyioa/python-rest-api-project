45557bdfd8d7b2f5800611409df816542c2ad9aa
Olufemi Isola <tobbyioa@gmail.com>
2023-08-03 18:18:44 -0600

    Added registration email and send email

--- resources/user.py
+++ resources/user.py
@@ -1,33 +1,67 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token, jwt_required,get_jwt, get_jwt_identity,create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt,
+    get_jwt_identity,
+    create_refresh_token,
+)
+from sqlalchemy import or_
+import requests
+import os
 
 from blocklist import BLOCKLIST
 
 from db import db
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegistrationSchema
 
 
 blp = Blueprint("Users", __name__, description="Operation on users")
 
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+        auth=("api", os.getenv("MAIKGUN_API_KEY")),
+        data={
+            "from": f"tobbyioa <mailgun@{domain}>",
+            "to": [to],
+            "subject": subject,
+            "text": body,
+        },
+    )
+
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    #@jwt_required(fresh=True)
-    @blp.arguments(UserSchema)
+    # @jwt_required(fresh=True)
+    @blp.arguments(UserRegistrationSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"],
+            )
+        ).first():
             abort(409, message="A User with that username already exists")
 
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"]),
         )
 
         db.session.add(user)
         db.session.commit()
+        send_simple_message(
+            to=user.email,
+            subject="User registreation",
+            body=f"Hi {user.username}! Registration successful",
+        )
 
         return {"message": "User successfully created"}, 201
 
@@ -46,22 +80,24 @@ class UserLogin(MethodView):
             return {"access_token": access_token, "refresh_token": refresh_token}
         abort(401, message="Invalid Credentials.")
 
+
 @blp.route("/refresh")
 class TokenRefresh(MethodView):
     @jwt_required(refresh=True)
     def post(self):
-        current_user =  get_jwt_identity()
-        new_token =  create_access_token(identity=current_user, fresh=False)
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
         return {"access_token": new_token}
         return {"access_token": new_token}
+
 
 @blp.route("/logout")
 class UserLogout(MethodView):
     @jwt_required()
     def post(self):
-        jti =  get_jwt()["jti"]
+        jti = get_jwt()["jti"]
         BLOCKLIST.add(jti)
-        return{"message": "Successfully logged out"}
+        return {"message": "Successfully logged out"}
 
 
 @blp.route("/user/<string:user_id>")
--- schemas.py
+++ schemas.py
@@ -49,3 +49,6 @@ class UserSchema(Schema):
     Id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True)
+    
+class UserRegistrationSchema(UserSchema):
+    email = fields.Str(required=True)
